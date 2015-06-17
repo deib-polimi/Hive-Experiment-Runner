5b165bd441220c188cb3a477b9360acf9da065dd
Eugenio Gianniti <egianniti@gmail.com>
2015-06-17 16:16:53 +0200

    Fix setup script

--- inst/setup.sh
+++ inst/setup.sh
@@ -7,17 +7,14 @@ fi
 
 isnumber() { test "$1" && printf '%d' "$1" > /dev/null 2>&1; }
 
-if [ ! isnumber $1 ]; then
-  echo "setup.sh: an integer is needed as SCALE" >&2
-  exit 2
-fi
+isnumber "$1" || ( echo "setup.sh: an integer is needed as SCALE" >&2; exit 2 )
 SCALE=$(printf '%d' "$1")
 
-SOURCE="${BASH_SOURCE[0]}"
+SOURCE="$0"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [ $SOURCE != /* ] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
