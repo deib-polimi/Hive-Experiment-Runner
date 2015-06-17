1ddf01c6aad9e91c54a31ddd0316171015be88f0
Eugenio Gianniti <egianniti@gmail.com>
2015-06-17 17:56:09 +0200

    Avoid deprecated flag in test

--- inst/setup.sh
+++ inst/setup.sh
@@ -11,10 +11,10 @@ isnumber "$1" || ( echo "setup.sh: an integer is needed as SCALE" >&2; exit 2 )
 SCALE=$(printf '%d' "$1")
 
 SOURCE="$0"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+while [ -L "$SOURCE" ]; do
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [ $SOURCE != /* ] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [ $SOURCE != /* ] && SOURCE="$DIR/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
