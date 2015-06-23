cd8b7cb16d1a860fb067c25f2850003d1cb84201
Eugenio Gianniti <egianniti@gmail.com>
2015-06-23 12:02:22 +0200

    Prepare script to launch several queues

---
+++ launchQueues.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+while read line; do
+  ${SCRIPT_DIR}/singleQueue.sh ${line} &
+done < ${SCRIPT_DIR}/config/ssdata.conf
