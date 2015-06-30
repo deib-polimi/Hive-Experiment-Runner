8e4686f8aa0b0e65e511b7d2f65ff2c51ecd2e86
Eugenio Gianniti <egianniti@gmail.com>
2015-06-30 18:28:58 +0200

    Obtain a working prepareGantt.py

--- prepareGantt.py
+++ prepareGantt.py
@@ -10,10 +10,18 @@ for line in open (os.path.join(sys.path[0], "config/variables.sh"), "r").read ()
       print "Working on the following queries: " + str (query_list)
       break
 
+do_debug = (os.getenv ("DEBUG", "no") == "yes")
+
 class Gantt:
+  gantts = 0
+  
   def __init__ (self):
+    Gantt.gantts += 1
+    if do_debug:
+      print "I'm Gantt number {}".format (Gantt.gantts)
     self.phases = list ()
-    self.tasks = dict ()
+    self.tasks = list ()
+    self.tasks_per_phase = dict ()
     self.starts = dict ()
     self.ends = dict ()
     self.durations = dict ()
@@ -21,31 +29,61 @@ class Gantt:
   def add_phase (self, phase_id):
     if phase_id not in self.phases:
       self.phases.append (phase_id)
+      if do_debug:
+        print "Added phase {}".format (phase_id)
+    else:
+      if do_debug:
+        print "Failed to add phase {}".format (phase_id)
   
   def add_task (self, phase_id, task_id):
     if phase_id in self.phases:
-      if phase_id not in self.tasks:
-        self.tasks[phase_id] = [task_id]
+      self.tasks.append (task_id)
+      if phase_id not in self.tasks_per_phase:
+        self.tasks_per_phase[phase_id] = [task_id]
       else:
-        self.tasks[phase_id].append (task_id)
+        self.tasks_per_phase[phase_id].append (task_id)
+      if do_debug:
+        print "Added task {} to phase {}".format (task_id, phase_id)
+    else:
+      if do_debug:
+        print "Failed to add task {} to phase {}".format (task_id, phase_id)
   
   def set_start_time (self, task_id, start_time):
-    if task_id in self.tasks.values ():
+    if task_id in self.tasks:
       self.starts[task_id] = start_time
+      if do_debug:
+        print "Added start time to task {}".format (task_id)
+    else:
+      if do_debug:
+        print "Failed to add start time to task {}".format (task_id)
   
   def set_end_time (self, task_id, end_time):
-    if task_id in self.tasks.values ():
+    if task_id in self.tasks:
       self.ends[task_id] = end_time
+      if do_debug:
+        print "Added end time to task {}".format (task_id)
+    else:
+      if do_debug:
+        print "Failed to add end time to task {}".format (task_id)
   
   def set_duration (self, task_id, duration):
-    if task_id in self.tasks.values ():
+    if task_id in self.tasks:
       self.durations[task_id] = duration
+      if do_debug:
+        print "Added duration to task {}".format (task_id)
+    else:
+      if do_debug:
+        print "Failed to add duration to task {}".format (task_id)
   
   def write_csv (self, file):
     file.write ("Phase,Task,Start,End,Duration\n")
     try:
       for phase in self.phases:
-        for task in self.tasks[phase]:
+        if do_debug:
+          print "Working on phase {}".format (phase)
+        for task in self.tasks_per_phase[phase]:
+          if do_debug:
+            print "Writing data of task {}".format (task)
           line = "{P},{T},{S},{E},{D}\n".format (P = phase, T = task,
                                                  S = self.starts[task],
                                                  E = self.ends[task],
@@ -74,53 +112,60 @@ for query in query_list:
     for job in vertex_order_file:
       gantt = Gantt ()
       for phase in job.strip ().split ("\t"):
-        gantt.add_phase (phase.strip ())
+        phase = phase.strip ()
+        if phase:
+          gantt.add_phase (phase)
       for line in vertex_lists_file:
+        line = line.strip ()
         if line:
           phase, separator, line = line.partition ("\t")
           phase = phase.strip ()
-          for task in line.strip (). split ("\t"):
-            gantt.add_task (phase, task.strip ())
+          for task in line.strip ().split ("\t"):
+            task = task.strip ()
+            if task:
+              gantt.add_task (phase, task)
         else:
           break
       for line in task_start_end_file:
+        line = line.strip ()
         if line:
           looking_for = "task"
           for word in line.strip ().split ("\t"):
+            word = word.strip ()
             if word:
               if looking_for == "task":
-                task = word.strip ()
+                task = word
                 looking_for = "start"
               elif looking_for == "start":
-                gantt.set_start_time (task, word.strip ())
+                gantt.set_start_time (task, word)
                 looking_for = "end"
               elif looking_for == "end":
-                gantt.set_end_time (task, word.strip ())
+                gantt.set_end_time (task, word)
                 break
         else:
           break
       for line in task_durations_file:
+        line = line.strip ()
         if line:
           looking_for = "task"
           for word in line.strip ().split ("\t"):
+            word = word.strip ()
             if word:
               if looking_for == "task":
-                task = word.strip ()
+                task = word
                 looking_for = "duration"
               elif looking_for == "duration":
-                gantt.set_duration (task, word.strip ())
+                gantt.set_duration (task, word)
                 break
         else:
           break
       filename = "gantt{0:06d}.csv".format (counter)
       counter += 1
-      gantt_file = open (os.path.join (gantts_dir, filename), "w")
-      try:
-        gantt.write_csv (gantt_file)
-      except RuntimeError:
-        print sys.exc_info ()[1]
-      finally:
-        gantt_file.close ()
+      with open (os.path.join (gantts_dir, filename), "w") as gantt_file:
+        try:
+          gantt.write_csv (gantt_file)
+        except RuntimeError:
+          print sys.exc_info ()[1]
   
   finally:
     vertex_order_file.close ()
