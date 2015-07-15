53bb847f007afcd41238966d780ded6e3e7cbaff
Eugenio Gianniti <egianniti@gmail.com>
2015-07-15 13:59:20 +0200

    Prepare configuration for 30 nodes and 250 GB dataset

--- config/variables.sh
+++ config/variables.sh
@@ -2,7 +2,7 @@
 # Just some variables useful for other scripts
 
 # Scale size for datagen
-SCALE=4
+SCALE=250
 # Number of external iteration for the single query
 EXTERNALITER=1
 # Number of internal iteration for the single query
@@ -12,9 +12,9 @@ ISPOLICLOUD=0
 # extension of the query script, typically sql
 QUERYEXTENSION=sql
 # The hostname of the node hosting the Resource Manager
-MASTER=slave1
+MASTER=slave10
 # A list of queries to execute in the single query run, they are the ones we will execute in the session mode
-QUERIES="R1 R2"
+QUERIES="R1 R2 R3 R4"
 # User for ssh-ing into other nodes
 CURUSER=ubuntu
 # Database to be used
@@ -22,6 +22,6 @@ DB_NAME="tpcds_text_$SCALE"
 # Path to Resource Manager log file
 LOG_PATH=/var/log/hadoop-yarn/yarn/@@RM_LOG@@
 # Host _hosting_ HiveServer2
-HIVE_SERVER2=slave2
+HIVE_SERVER2=slave11
 # Maximum number of RM logs fetch attempts
 FETCH_ATTEMPTS=60
