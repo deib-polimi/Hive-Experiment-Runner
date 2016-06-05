2ebb98bd4ead883fff9fc8f869bf70fca3001b5a
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-06-05 12:13:39 +0200

    Convert dtsat-related functions to Octave

--- dstat/csv2struct.m
+++ dstat/csv2struct.m
@@ -1,87 +1,79 @@
-%% Copyright 2016 Saeid Shams
-% 
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-% 
-%     http://www.apache.org/licenses/LICENSE-2.0
-% 
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
+## Copyright 2016 Saeid Shams
+## Copyright 2016 Eugenio Gianniti
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
 
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{stats} =} csv2struct (@var{filename})
+##
+## Read dstat data from @var{filename} and return it as a struct, @var{stats}.
+##
+## @seealso{drawplot}
+## @end deftypefn
 
+function stats = csv2struct (filename)
 
-function stats_struct = csv2struct(filename)
+if (nargin == 1)
+  if (ischar (filename))
+    header_rows = 7;
+    delimiter = ",";
 
-    %   csv2struct function imports and reads a .csv file with a single line of comma separated
-    %   text headers followed by many rows of numeric data into a structure array.
-    %   Run --> type code to run:
-    %   Example:
-    %   dstats = csv2struct('stats.master1.csv');
+    fid = fopen (filename, "r");
+    fskipl (fid, header_rows - 1);
 
-    if nargin == 1
-        startRow = 7;
-        delimiter = ',';
-    else
-        error('csv2struct requires one argument');
-    end
-    
-    fileID = fopen(filename,'r');
-    
-    for i = 1:startRow
-        headers = fgetl(fileID);
-    end
-    
-    % Parse the header string (6th row) into separate headers.
-    headcell = textscan(headers ,'%s','delimiter',delimiter);
-    
-    % In general cell arrays work just like N-dimensional arrays with the exception of 
-    % the use of ‘{’ and ‘}’ as allocation and indexing operators.
+    % Parse the header string (6th row) into separate headers
+    headers = fgetl (fid);
+    headcell = textscan (headers, "%s", "delimiter", delimiter);
     headcell = headcell{1};
-    
+
     % Read the first line of data (7th row) to determine the column data types
-    first_row = fgetl(fileID);
-    data = textscan(first_row,'%s',length(headcell),'delimiter',delimiter);
+    first_row = fgetl (fid);
+    data = textscan (first_row, "%s", length (headcell), "delimiter", delimiter);
     data = data{1};
-    
-    stats_struct = [];
-    
-    strFormat = [];
-    for i = 1:length(data)
-        % If str2double returns a numeric value, so the column will be numeric,
-        % otherwise if str2double returns empty, so the column will be text
-        if ~isnan(str2double(data{i}))
-            colFormat = '%f ';
-        else
-             colFormat = '%s ';
-        end
-        strFormat = [strFormat colFormat];  %#ok<AGROW>
-    end
-    
-    % restart from the first line of file
-    frewind(fileID);
-    
-    % use the new column format to read the file
-    data = textscan(fileID,strFormat,'delimiter',delimiter,'headerlines',startRow);
-    fclose(fileID);
-    
-    for i = 1:length(data)
-        
-        header = headcell{i};
-        
-        % Remove ( " ) parts of headers
-        header = strrep(header,'"','');
-        
-        if i ~= 1   
-            stats_struct.(header) = data{i};
-        else
 
-           data{i} = datenum(data{i} , 'dd-mm HH:MM:SS');  
-           stats_struct.(header) = data{i};
+    row_content = [];
+    for (ii = 1:length (data))
+      if (! isnan (str2double (data{ii})))
+        row_content = [row_content, "%f "];
+      else
+        row_content = [row_content, "%s "];
+      endif
+    endfor
 
-        end
-    end
-  endfunction
+    frewind (fid);
+    data = textscan (fid, row_content, "delimiter", delimiter, "headerlines", header_rows);
+    fclose (fid);
+
+    stats = struct();
+    for (ii = 1:length (data))
+      header = headcell{ii};
+
+      if (header(1) == header(end) && header(end) == '"')
+        header = header(2:end - 1);
+      endif
+
+      if (ii > 1)
+        stats.(header) = data{ii};
+      else
+        data{ii} = datenum (data{ii} , "dd-mm HH:MM:SS");
+        stats.(header) = data{ii};
+      endif
+    endfor
+  else
+    error("csv2struct: you should provide a filename as input");
+  endif
+else
+  error("csv2struct requires one argument");
+endif
+
+endfunction
--- dstat/drawplot.m
+++ dstat/drawplot.m
@@ -1,48 +1,50 @@
-%% Copyright 2016 Saeid Shams
-% 
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-% 
-%     http://www.apache.org/licenses/LICENSE-2.0
-% 
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
+## Copyright 2016 Saeid Shams
+## Copyright 2016 Eugenio Gianniti
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
 
-function drawplot(strt , varargin)
+## -*- texinfo -*-
+## @deftypefn {Function File} {} drawplot (@var{stats}, @var{varnames})
+##
+## This function receives as input the @var{stats} struct and the string arguments.
+## The first argument after struct must be a time.
+##
+## @seealso{csv2struct}
+## @end deftypefn
 
-    % This function receives as input a struct and the string argoments.
-    % The first argoment after struct must be a time.
-    %   Type code to run:
-    %   Example:      
-    %         drawplot(strt,'time','usr','sys','idl','wai')
+function drawplot (stats, varargin)
 
-    if ~isstruct(strt)
-      msgerror = strcat('''',inputname(1),''''' is not a structure!!');
-      error(msgerror);
-    end
-%   varargin = cell 1x4 : varargin{1,1}, varargin{1,2}....
-    c = struct2cell(strt); %15x1 cell array value= c{1,1},c{2,1},...
-    varnames = fieldnames(strt); % 15x1 cell names , time=varnames{1,1} , usr=varnames{2,1},..
+if (isstruct (stats))
+  c = struct2cell (stats);
+  varnames = fieldnames (stats);
 
-    cellLegend = cell(1 , length(varargin)-1);  %initialize of cell array to use in Legend().
-    if strcmp(varargin{1,1} , varnames{1,1}) == 0  % the first argoment after struct must be 'time' .
-        msgerror = strcat('''',varargin{1,1},''''' is not a time!! The first argoment after struct must be time.');
-        error(msgerror);
-    else
-        for i=2:length(varargin)  % i=2 because the first argoment is time.
-                idx = find(strcmp(varargin{1,i} , varnames));
-                if ~isempty(idx)
-                    stem(c{1,1},c{idx}  , 'LineWidth',3); 
-                    datetick('x','HH:MM');
-                    hold all;
-                    cellLegend(1,i-1) = varnames(idx,1);
-                    hold all;
-                end
-         end
-         legend (cellLegend, 'location', 'southwestoutside');
-    end
+  cell_legend = cell (1 , nargin - 2);
+  if (strcmp (varargin{1, 1} , varnames{1, 1}) == 0)
+    error ("drawplot: the second argument must be ""time""");
+  else
+    for (ii = 2:nargin - 1)
+      idx = find (strcmp (varargin{1, ii} , varnames));
+      if (! isempty (idx))
+        stem (c{1, 1}, c {idx}, "LineWidth", 3);
+        datetick ("x", "HH:MM");
+        hold all;
+        cell_legend(ii - 1) = varnames(idx, 1);
+      endif
+    endfor
+    legend (cell_legend, "location", "southwestoutside");
+  endif
+else
+  error ("drawplot: STATS should be a struct");
+endif
 
+endfunction
